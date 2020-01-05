a14154ce44539adb020e48c436de55e5ad2d4674
Pedro Augusto <pams.pedro@gmail.com>
2020-01-05 01:17:46 -0300

    feat: install laravelha preset-api

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,7 +7,20 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Server(url="/api"),
+ * @OA\Info(title="Laravelha API", version="0.0.1")
+ */
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+    *  Redirect to api documentation
+    */
+    public function apiDoc()
+    {
+        return redirect('api/doc');
+    }
 }
